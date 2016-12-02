483a02ad467d31088a3e9903538b130d028ce149
oroni <brianoroni6@gmail.com>
2016-12-02 10:35:19 +0300

    booking app

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.tenants, name="tenants"),
-    url(r'^tenant-cashflows/(?P<tenant_id>\d+)/$',
-        views.tenant_cashflows, name="tenant_cashflows"),
+    url(r'^tenant-cashflows/(?P<tenant_id>\d+)/$',views.tenant_cashflows, name="tenant_cashflows"),
 )
--- proprio/settings.py
+++ proprio/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'main',
-    'bank_import',
+    'booking',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
